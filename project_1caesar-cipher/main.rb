c244b2f4cf1e1cd42af14f31252b0a6b3d11973a
Zarifa <mamadalievazara@gmail.com>
2025-10-12 17:57:53 +0200

    Remove: inspect and return the encoded string.

--- project_1caesar-cipher/main.rb
+++ project_1caesar-cipher/main.rb
@@ -18,7 +18,30 @@ def caesar_cipher(string, num)
       result.push(char)
     end
   end
-  p "Now you can decode => #{result.join("")}"
+  "Now you can decode => #{result.join("")}"
+end
+
+caesar_cipher(input, 5)
+puts "Enter your secret message here: "
+input = gets.chomp.chars()
+
+
+def caesar_cipher(string, num)
+  result = []
+  string.each do |char| 
+    code = char.ord
+    shifted = code + num
+    if code.between?(97, 122)
+      wrapped = ((shifted - 97) % 26) + 97
+      result.push(wrapped.chr)
+    elsif code.between?(65, 90)
+      wrapped = ((shifted - 65) % 26) + 65
+      result.push(wrapped.chr)
+    else
+      result.push(char)
+    end
+  end
+  "Now you can decode => #{result.join("")}"
 end
 
 caesar_cipher(input, 5)
